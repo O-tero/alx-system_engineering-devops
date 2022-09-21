759d56be9b45220a6cae6feb932d56a0101c9903
O-tero <oteronickbrian@gmail.com>
2022-09-21 22:30:58 +0300

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# install puppet-lint
+# Using Puppet, install flask from pip3.
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
